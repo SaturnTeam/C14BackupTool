05706d631ee54de86f499302fa303235d6ee6664
TheSaturn <saturn@thesaturn.me>
2017-06-02 08:29:37 +0700

    tipo
    Signed-off-by: TheSaturn <saturn@thesaturn.me>

--- src/BackupHandler.php
+++ src/BackupHandler.php
@@ -251,7 +251,7 @@ class BackupHandler
         {
             exec($cmd, $output, $returnCode);
             sleep(1);//yep, i's necessary
-            if ($returnCode === 0 || isset($output[count($output)-2]) && $output[count($output)-2] == 'fuse: mountpoint is not empty')
+            if ($returnCode === 0 || isset($output[count($output)-2])] && $output[count($output)-2] == 'fuse: mountpoint is not empty')
             {
                 return true;
             }
